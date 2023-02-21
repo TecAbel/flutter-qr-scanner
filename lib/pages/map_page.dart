4df02f7e1b1455705c8cae32ffc1579a0b2e2156
TecAbel <abelardo96.work@gmail.com>
2023-02-21 13:03:00 -0600

    feat(google_maps): google maps implementation for android devices
    BREAKING CHANGE: added google maps for android

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,10 +1,26 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:qr_scanner/config/app_theme.dart';
 import 'package:qr_scanner/models/qr_model.dart';
 
-class MapPage extends StatelessWidget {
+class MapPage extends StatefulWidget {
   const MapPage({super.key});
+
+  @override
+  State<MapPage> createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  final Completer<GoogleMapController> _controller =
+      Completer<GoogleMapController>();
+
+  final CameraPosition initialPosition = const CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -29,7 +45,14 @@ class MapPage extends StatelessWidget {
         ),
         backgroundColor: CustomAppTheme.primaryColor,
       ),
-      body: Center(child: Text(qr.value)),
+      // body: Center(child: Text(qr.value)),
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: initialPosition,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
     );
   }
 }
